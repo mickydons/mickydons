5f839ebba3444039351b863ea94c28fa92408272
shizzy03 <scholarship100012@gmail.com>
2026-09-05 18:14:08 +0100

    Commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,18 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${manrope.variable} ${dmSerifDisplay.variable}`}>
       <head>
+        {/* Google Tag Manager - GTM-5Q6LQMZX */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5Q6LQMZX');
+          `}
+        </Script>
+        {/* End Google Tag Manager */}
+
         {/* Google tag (gtag.js) - AW-18141846129 */}
         <Script 
           async 
@@ -44,28 +56,16 @@ export default function RootLayout({
             gtag('config', 'AW-18141846129');
           `}
         </Script>
-
-        {/* Google Tag Manager - GTM-TJ56BMBX */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-TJ56BMBX');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
         
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=DM+Serif+Display&family=Manrope:wght@300;400;500;600;700;800&display=swap" rel="stylesheet" />
       </head>
       <body className="font-body antialiased selection:bg-primary/30 selection:text-primary-foreground">
-        {/* Google Tag Manager (noscript) - Fallback for every page */}
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TJ56BMBX"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5Q6LQMZX"
             height="0" 
             width="0" 
             style={{ display: 'none', visibility: 'hidden' }}
